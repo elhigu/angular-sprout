2f9f1fa89ef30069a4e0f3e29e5b5d211b7b4b0c
Mikael Lepistö <mikael.lepisto@vincit.fi>
2014-05-09 16:34:28 +0300

    Added mock implementation for angular-translate.

--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -5,7 +5,30 @@
  * automatically.
  */
 describe( 'home section', function() {
-  beforeEach( module( 'pascalprecht.translate' ) );
+
+  // Mock angular-translate to return key
+  beforeEach(function () {
+    module('pascalprecht.translate');
+    module(function ($provide) {
+      $provide.decorator('$translate', function($delegate) {
+        var mock_translate = function (key) {
+          var kind_of_promise = {};
+          kind_of_promise.then = function (callback) {
+            callback(key);
+            return kind_of_promise;
+          };
+          console.log("Called mock $translate with key", key);
+          return kind_of_promise;
+        };
+        mock_translate.instant = function (key) { return key; };
+        mock_translate.storageKey = function () {};
+        mock_translate.storage = function () {};
+        mock_translate.preferredLanguage = function () {};
+        return mock_translate;
+      });
+    });
+  });
+
   beforeEach( module( 'app.home' ) );
 
   it( 'should have a dummy test', inject( function() {
