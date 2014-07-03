59fb1d9cc47a6255cae979c33c8d9dfb028b2dd0
Mikael Lepistö <mikael.lepisto@vincit.fi>
2014-07-03 22:45:26 +0300

    Added yet more tests for betterpicker.

--- src/common/angular-ui-bootstrap-fixes/betterpicker.spec.js
+++ src/common/angular-ui-bootstrap-fixes/betterpicker.spec.js
@@ -41,7 +41,11 @@ describe( 'Better datepicker directive', function() {
       $compile = $injector.get('$compile');
 
       $rootScope.pickerState = {};
-      var defaultTemplate = '<betterpicker ng-model="selectedDate" state="pickerState"></betterpicker>';
+      $rootScope.date = {};
+      var defaultTemplate = '<div>' +
+        '<div>{{date.selectedDate}}</div>' + 
+        '<betterpicker ng-model="date.selectedDate" state="pickerState"></betterpicker>' +
+        '</div>';
       initDirective(defaultTemplate);
     })
   );
@@ -86,32 +90,57 @@ describe( 'Better datepicker directive', function() {
       testTools.click(button);
       $rootScope.$digest();
       expect( jqEl.find('.pickerCalendar').length ).toBe(1);
-      expect($rootScope.selectedDate).toBeTruthy();
+      expect($rootScope.date.selectedDate).toBeTruthy();
       expect(inputEl.val()).toBeTruthy();
     });
 
-    it( 'should clear input field and model when "clear" is clicked',function() {
-      $rootScope.selectedDate = new Date();
+    it( 'should clear input field and model and close picker when "clear" is clicked',function() {
+      $rootScope.date.selectedDate = new Date();
       var clearBtn = jqEl.find('button.bpBtnClear');
       var inputEl = jqEl.find('input');
+      $rootScope.$digest();
       testTools.click(clearBtn);
       $rootScope.$digest();
       expect( jqEl.find('.pickerCalendar').length ).toBe(0);
-/* For some reason test does not set model to null... yet...
-      testTools.showHtml(jqEl.html());
-      console.log("Test scope failing:", $rootScope);
       expect(inputEl.val()).toBeFalsy();
-      expect($rootScope.selectedDate).toBe(null);
-*/
+      expect($rootScope.date.selectedDate).toBe(null);
     });
 
     it( 'should close calendar and not change input / model if "close" is clicked',function() {
+      var testDate = new Date();
+      $rootScope.date.selectedDate = testDate;
+      var clearBtn = jqEl.find('button.bpBtnClose');
+      var inputEl = jqEl.find('input');
+      $rootScope.$digest();
+      var inputVal = inputEl.val();
+
+      testTools.click(clearBtn);
+      $rootScope.$digest();
+      expect( jqEl.find('.pickerCalendar').length ).toBe(0);
+      expect(inputEl.val()).toBe(inputVal);
+      expect( moment($rootScope.date.selectedDate).format('YYYYMMDD') )
+      .toBe(moment(testDate).format('YYYYMMDD'));        
     });
 
     it( 'should not close picker and update datePickerDate when formattedDate changes',function() {
+      var dateStr = '2010-10-10';
+      var inputEl = jqEl.find('input');
+      testTools.setVal(inputEl, dateStr);
+      $rootScope.$digest();
+      expect( jqEl.find('thead').html() ).toContain('2010');
+      expect( jqEl.find('table').length ).toBe(1); // for some reason jquery failed selecting .pickerCalendar
+      expect( moment($rootScope.date.selectedDate).format('YYYY-MM-DD') ).toBe(dateStr);
     });
 
-    it( 'should not change ngModel / datePickerDate if formatted date is changed to invalid',function() {
+    it( 'should not change ngModel if formatted date is changed to invalid',function() {
+      var dateStr = '2010-10-x';
+      var inputEl = jqEl.find('input');
+      var testDate = new Date();
+      $rootScope.date.selectedDate = testDate;
+      testTools.setVal(inputEl, dateStr);
+      $rootScope.$digest();
+      expect( moment($rootScope.date.selectedDate).format('YYYYMMDD') )
+      .toBe(moment(testDate).format('YYYYMMDD'));
     });
   });
 
